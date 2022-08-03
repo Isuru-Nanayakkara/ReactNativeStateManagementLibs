8b80986dc97a978bc8bbea4b574153152d07d75b
Isuru Nanayakkara <isuru.nan@gmail.com>
2022-08-03 19:02:18 +0530

    Created individual section in App.js for each library.

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
 import {NavigationContainer} from '@react-navigation/native';
-// import React, {useReducer} from 'react';
 import AppStack from './src/navigation';
 
-// When using Redux
+/************************************************/
+// Uncomment below section for normal behavior without libraries
+/************************************************/
+import React from 'react';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
+};
+
+/************************************************/
+// Uncomment below section when trying Redux
+/************************************************/
+// import React from 'react';
 // import {Provider} from 'react-redux';
 // import {PersistGate} from 'redux-persist/integration/react';
 // import Store from './src/providers/reduxCode';
 
-// When using Context API
-import React, {useReducer} from 'react';
-import {
-  counter,
-  CounterContext,
-  initialState,
-} from './src/providers/contextAPICode';
-
-/**********************************************/
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <AppStack />
-//     </NavigationContainer>
-//   );
-// };
-
-// When using Redux
 // const App = () => {
 //   const {reduxStore, persister} = Store();
 
@@ -40,17 +36,26 @@ import {
 //   );
 // };
 
-// When using Context API
-const App = () => {
-  const [state, dispatch] = useReducer(counter, initialState);
+/************************************************/
+// Uncomment below section when trying Context API
+/************************************************/
+// import React, {useReducer} from 'react';
+// import {
+//   counter,
+//   CounterContext,
+//   initialState,
+// } from './src/providers/contextAPICode';
 
-  return (
-    <CounterContext.Provider value={[state, dispatch]}>
-      <NavigationContainer>
-        <AppStack />
-      </NavigationContainer>
-    </CounterContext.Provider>
-  );
-};
+// const App = () => {
+//   const [state, dispatch] = useReducer(counter, initialState);
+
+//   return (
+//     <CounterContext.Provider value={[state, dispatch]}>
+//       <NavigationContainer>
+//         <AppStack />
+//       </NavigationContainer>
+//     </CounterContext.Provider>
+//   );
+// };
 
 export default App;
