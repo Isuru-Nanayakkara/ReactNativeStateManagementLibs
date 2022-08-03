c43f11656ae5ba944d20910f6da5dc177dc69bc6
Isuru Nanayakkara <isuru.nan@gmail.com>
2022-08-03 23:34:47 +0530

    Code cleanup.

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import AppStack from './src/navigation';
 /************************************************/
 // Uncomment below section when testing useState, Hookstate, MobX
 /************************************************/
-// import React from 'react';
+import React from 'react';
 
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <AppStack />
-//     </NavigationContainer>
-//   );
-// };
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
+};
 
 /************************************************/
 // Uncomment below section when testing Redux
@@ -78,20 +78,20 @@ import AppStack from './src/navigation';
 /************************************************/
 // Uncomment below section when testing recoil
 /************************************************/
-import React, {Suspense} from 'react';
-import {Text} from 'react-native';
-import {RecoilRoot} from 'recoil';
+// import React, {Suspense} from 'react';
+// import {Text} from 'react-native';
+// import {RecoilRoot} from 'recoil';
 
-const App = () => {
-  return (
-    <RecoilRoot>
-      <Suspense fallback={<Text>Loading...</Text>}>
-        <NavigationContainer>
-          <AppStack />
-        </NavigationContainer>
-      </Suspense>
-    </RecoilRoot>
-  );
-};
+// const App = () => {
+//   return (
+//     <RecoilRoot>
+//       <Suspense fallback={<Text>Loading...</Text>}>
+//         <NavigationContainer>
+//           <AppStack />
+//         </NavigationContainer>
+//       </Suspense>
+//     </RecoilRoot>
+//   );
+// };
 
 export default App;
